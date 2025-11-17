4535df662cc68ad3f776545cb74371e3248666e8
S-furi <stefano.furi@jetbrains.com>
2025-11-17 14:38:34 +0100

    chore: add benchmark deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     val kotlinVersion = libs.versions.kotlin.get()
     kotlin("jvm") version kotlinVersion
+    kotlin("plugin.allopen") version kotlinVersion
+
+    alias(libs.plugins.kotlinx.benchmark)
 }
 
 group = "io.github.sfuri"
@@ -15,9 +18,12 @@ repositories {
 dependencies {
     implementation(kotlin("reflect"))
     implementation(libs.logback.classic)
+    implementation(libs.kotlinx.benchamrk.runtime)
+    implementation(libs.kotlinx.coroutines.core)
 
     testImplementation(kotlin("test"))
     testImplementation(libs.junit.jupiter.api)
+    testImplementation(libs.kotlinx.coroutines.test)
     testRuntimeOnly(libs.junit.jupiter.engine)
 }
 
@@ -27,6 +33,10 @@ tasks.test {
 
 kotlin {
     jvmToolchain(21)
+}
+
+allOpen {
+    annotation("org.openjdk.jmh.annotations.State")
 }
 
 tasks.withType<KotlinCompile>().configureEach {
